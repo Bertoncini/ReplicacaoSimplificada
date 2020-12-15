e9ce621a33eef02d8995ab90e44e88d099003c49
Alexsandro <alex.bertoncini@terra.com.br>
2020-12-15 07:46:06 -0300

    Adicionado projeto de teste e poc para verificar as funcionalidades da dll

---
+++ POC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace POC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ReplicacaoSimplificada.Program.Main(new string[] {
+                "sourceserver", "serveSource", "sourcedatabase","database", "sourceschema","schema", "sourcetable","table", "sourceuser","user_source", "sourcepassword","password_source",
+                "destinationserver","serveDestination", "destinationdatabase","database", "destinationschema","schema", "destinationtable","table", "destinationuser","user_destination", "destinationpassword","password_destination"});
+
+            Console.WriteLine(ReplicacaoSimplificada.Program.Mensagem);
+            Console.WriteLine(ReplicacaoSimplificada.Program.SeAtualizado);
+        }
+    }
+}
